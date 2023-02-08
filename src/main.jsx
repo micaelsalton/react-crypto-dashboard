a4dc6490bd0d2d09b456ff28a0e1d8834504d2fc
Micael Salton <105461630+micaelsalton@users.noreply.github.com>
2023-02-08 17:45:26 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,11 @@ import { ContextProvider } from './Context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <ContextProvider>
       <Router>                
         <App />
       </Router>
-    
+    </ContextProvider>
   </React.StrictMode>
 );
 
